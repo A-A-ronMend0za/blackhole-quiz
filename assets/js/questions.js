e8db01b81625a869c1d2deeac91e5c279ad884b1
A-A-ronMend0za <aaronmendoza.denver@gmail.com>
2024-09-24 21:51:37 -0600

    questions populated

---
+++ assets/js/questions.js
@@ -0,0 +1,47 @@
+var questions = [
+  {
+    title: "What is the name of Our Blackhole?",
+    options: ["Sagittarius A", "Sagittarius B", "Andromeda", "Arcturus"],
+    answer: "Sagittarius A",
+  },
+  {
+    title: "What is NOT a category of Blackhole?",
+    options: ["Primordial", "Stellar", "Superliminal", "Supermassive"],
+    answer: "Superliminal",
+  },
+  {
+    title: "How far away is our closest Blackhole?",
+    options: [
+      "16,000 light-years",
+      "1,600 light-years",
+      "2,600 light-years",
+      "26,000 light-years",
+    ],
+    answer: "26,000 light-years",
+  },
+  {
+    title: "What is the Edge of a Blackhole called?",
+    options: [
+      "Black Horizon",
+      "Event Horizon",
+      "Bring me the Horizon",
+      "Horizon Ending",
+    ],
+    answer: "Event Horizon",
+  },
+  {
+    title: "What can escape a Blackhole?",
+    options: [
+      "Hawking Particles",
+      "Thermal Radiation",
+      "Hawking Radiation",
+      "Nothing",
+    ],
+    answer: "Hawking Radiation",
+  },
+  {
+    title: "What year was the first Blackhole Photographed?",
+    options: ["1971", "1983", "2004", "2019"],
+    answer: "2019",
+  },
+];
